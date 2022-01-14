e9b30ab23b3320342b4b9bea6af964590033ed64
wkdqudans <jbm@vr-jamong.com>
2022-01-14 17:09:17 +0900

    장병문 : Cut 업로드

---
+++ Homework220114_5/Homework220114_5.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+void Cut(const char* _Text, int _Start, int _End, char* _Result)
+{
+	int iCount = 0;
+	for (int i = _Start; i <= _End; i++)
+	{
+		_Result[iCount] = _Text[i];
+		iCount += 1;
+	}
+}
+
+int main()
+{
+	char Text[100] = { };
+	Cut("0123456789", 2, 5, Text);
+	std::cout << Text << std::endl;
+}
