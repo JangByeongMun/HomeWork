4b2fc7886b1b03298bb07780b1e2ccc3fe5ab7fa
wkdqudans <jbm@vr-jamong.com>
2022-01-14 15:15:31 +0900

    장병문 : 기존 숙제들 업로드

---
+++ HomeWork220112/HomeWork220112.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+int main()
+{
+	{   // 배열에 1부터 짝수로 가득 채우기.
+		int arr[10] = {0};
+		int index = 0;
+		while (10 > index)
+		{
+			arr[index] = 2 * (index + 1);
+			index += 1;
+		}
+
+		int a = 0;
+	}
+
+	{	// 배열에 1부터 홀수로 가득 채우기
+		int arr[10] = { 0 };
+		int index = 0;
+		while (10 > index)
+		{
+			arr[index] = (2 * index) + 1;
+			index += 1;
+		}
+
+		int a = 0;
+	}
+}
---
+++ HomeWork220113/HomeWork220113.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+// 공백을 제거하는 함수
+void StringTrimRemove(const char* _left, char* _right)
+{
+	int countL = 0;
+	int countR = 0;
+	while (0 != _left[countL])
+	{
+		if (' ' != _left[countL])
+		{
+			_right[countR] = _left[countL];
+			countR += 1;
+		}
+
+		countL += 1;
+	}
+}
+
+int main()
+{
+	char text[10000] = { 0 };
+	StringTrimRemove("a    df     s   g    h  j  k     ", text);
+
+	printf_s(text);
+}
