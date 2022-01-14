8c53792f562261ca544c49bec134bda8c199dbf6
wkdqudans <jbm@vr-jamong.com>
2022-01-14 16:49:09 +0900

    장병문 : CharChange 업로드

---
+++ Homework220114_3/Homework220114_3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+void CharChange(char* _Text, char _Prev, char _Next)
+{
+	int iCount = 0;
+	while (0 != _Text[iCount])
+	{
+		if (_Text[iCount] == _Prev)
+		{
+			_Text[iCount] = _Next;
+		}
+		iCount++;
+	}
+}
+
+int main()
+{
+	char Text[100] = "aa, bb, cc, dd , ee , fd , aa";
+
+	CharChange(Text, ',', '|');
+
+	printf_s(Text);
+}
