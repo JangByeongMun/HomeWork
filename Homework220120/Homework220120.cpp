b8e08c74cb24596a6218935ec8dd84670f936aa0
wkdqudans <jbm@vr-jamong.com>
2022-01-26 18:06:06 +0900

    장병문 : 22.01.26 ++, -- 구현

--- Homework220120/Homework220120.cpp
+++ Homework220120/Homework220120.cpp
@@ -13,11 +13,21 @@ class Test2
 class Test3
 {
 public:
-    char z[3] = {0};
+    char z = {0};
     int a = 0;
     char b = 0;
     char c = 0;
     char d = 0;
+    __int64 e = 0;
+};
+
+class Test4
+{
+public:
+    char a;
+    Test3 b;
+    int c;
+    char d;
 };
 
 int main()
@@ -28,11 +38,25 @@ int main()
 
     Test3 newTest3 = Test3();
     __int64 startValue1 = (__int64) & newTest3;
-    __int64 startValue2 = (__int64) & newTest3.z[0];
-    __int64 startValue3 = (__int64) & newTest3.z[1];
-    __int64 startValue4 = (__int64) & newTest3.z[2];
+    //__int64 startValue2 = (__int64) & newTest3.z[0];
+    //__int64 startValue3 = (__int64) & newTest3.z[1];
+    //__int64 startValue4 = (__int64) & newTest3.z[2];
+    __int64 startValue10 = (__int64)&newTest3.z;
     __int64 startValue5 = (__int64) & newTest3.a;
     __int64 startValue6 = (__int64) & newTest3.b;
     __int64 startValue7 = (__int64) & newTest3.c;
     __int64 startValue8 = (__int64) & newTest3.d;
+    __int64 startValue9 = (__int64) & newTest3.e;
+
+
+    int TestSize4 = sizeof(Test4);
+    Test4 newText4 = Test4();
+    __int64 startValue2_0 = (__int64)&newText4;
+    __int64 startValue2_1 = (__int64)&newText4.a;
+    __int64 startValue2_2 = (__int64)&newText4.b;
+    __int64 startValue2_3 = (__int64)&newText4.c;
+    __int64 startValue2_4 = (__int64)&newText4.d;
+
+    Test4 testArr[10] = {};
 }
+6.
---
+++ Homework220126/Homework220126.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+
+class MyInt
+{
+public:
+    int Value;
+
+public:
+    // 전위
+    MyInt& operator++()
+    {
+        this->Value += 1;
+        return *this;
+    }
+
+    // 후위
+    MyInt operator++(int)
+    {
+        MyInt beforeMyInt = MyInt(this->Value);
+        this->Value += 1;
+        return beforeMyInt;
+    }
+
+    // 전위
+    MyInt& operator--()
+    {
+        this->Value -= 1;
+        return *this;
+    }
+
+    // 후위
+    MyInt operator--(int)
+    {
+        MyInt beforeMyInt = MyInt(this->Value);
+        this->Value -= 1;
+        return beforeMyInt;
+    }
+
+public:
+    MyInt(int _Value)
+        : Value(_Value)
+    {
+
+    }
+};
+
+
+
+int main()
+{
+    int Value = 0;
+    int Result = 0;
+
+    Result = ++Value;
+    Result = 0;
+    Result = Value++;
+
+
+    MyInt MyValue = 0;
+    MyInt MyResult = 0;
+
+    MyResult = ++MyValue;
+    MyResult = 0;
+    MyResult = MyValue++;
+
+    MyResult = --MyValue;
+    MyResult = 0;
+    MyResult = MyValue--;
+}
