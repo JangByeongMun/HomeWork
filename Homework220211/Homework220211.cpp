3ed639bbc138cc5eeea708b9a97a8a21a93a49a0
wkdqudans <jbm@vr-jamong.com>
2022-02-11 15:44:11 +0900

    장병문 : 220211_operator( )( ) 구현

---
+++ Homework220211/Homework220211.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+class A
+{
+public:
+    A& operator()()
+    {
+        return *this;
+    }
+};
+
+class B
+{
+public:
+    B(A _a)
+    {
+
+    }
+};
+
+int main()
+{
+    A NewA;
+    B NewB(NewA());
+}
