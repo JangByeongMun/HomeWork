b75395e5b3d36a66f2d5016c9dba71c5d539a822
wkdqudans <jbm@vr-jamong.com>
2022-02-21 17:15:39 +0900

    장병문 : Leak 체크

---
+++ Test/Test.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <list>
+#include <conio.h>
+
+class Player
+{
+public:
+	Player();
+	~Player();
+
+	std::list<int*> AllBody;
+};
+Player::Player()
+{
+}
+
+Player::~Player()
+{
+}
+
+int main()
+{
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	//_CrtSetBreakAlloc(157);
+
+	Player* testPlayer = new Player();
+
+	delete testPlayer;
+	//testPlayer = nullptr;
+}
