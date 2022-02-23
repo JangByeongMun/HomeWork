91a36606e870fa1585753016b8315ae00cd4d34f
wkdqudans <jbm@vr-jamong.com>
2022-02-23 15:21:19 +0900

    장병문 : 재귀함수 종료 구현

---
+++ Homework220223/Homework220223.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int Value = 0;
+void Func()
+{
+    if (Value >= 30)
+    {
+        return;
+    }
+
+    std::cout << ++Value << std::endl;
+    Func();
+}
+
+int main()
+{
+    Func();
+}
