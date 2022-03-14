d23208958224379f5c46fdd7433dd7115a3ad869
장병문 <jundead7981@gmail.com>
2022-03-14 20:15:41 +0900

    장병문 : char배열과 "" 비교

---
+++ Homework220314/Homework220314.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main()
+{
+	char TestArr[10] = "Test";
+
+	if ("Test" == TestArr)
+	{
+		int a = 0;
+	}
+	else
+	{
+		int a = 0;
+	}
+	// TestArr는 char포인터형이므로 주소값과 "Test"는 문자열로 취급하기 때문에 값 == 주소 이므로 false가 나온다.
+}
