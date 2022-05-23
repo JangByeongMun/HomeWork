15978155f0dd47fb4525ac72def5ecfc2fde50aa
장병문 <jundead7981@gmail.com>
2022-05-23 15:29:41 +0900

    장병문 : 육망성 자전 및 공전 구현

---
+++ Homework220523/GameEngineCore/GameEngineRenderer.cpp
@@ -0,0 +1,83 @@
+#include "GameEngineRenderer.h"
+#include "GameEngineActor.h"
+#include "GameEngineLevel.h"
+#include <Windows.h>
+#include <GameEngineBase/GameEngineWindow.h>
+
+#include "GameEngineVertexBuffer.h"
+#include "GameEngineIndexBuffer.h"
+
+GameEngineRenderer::GameEngineRenderer() 
+{
+}
+
+GameEngineRenderer::~GameEngineRenderer() 
+{
+}
+
+
+void GameEngineRenderer::Start() 
+{
+	GetActor()->GetLevel()->PushRenderer(this);
+}
+
+float4 XDir = { 1, 0 };
+float4 YDir = { 0, -1 };
+
+float Angle = 0.0f;
+float Dis = 0.0f;
+float4 Postion;
+
+void GameEngineRenderer::Render(float _DeltaTime)
+{
+	float4 Dir = float4::DegreeToDirection2D(Angle);
+	Angle += _DeltaTime * 360.0f;
+	GetActor()->GetTransform().SetPosition(float4{ 300.0f, 300.0f } + Dir * 100.0f);
+
+	// 랜더링
+	GameEngineVertexBuffer* Vertex = GameEngineVertexBuffer::Find("Hexagram");
+	GameEngineIndexBuffer* Index = GameEngineIndexBuffer::Find("Hexagram");
+	
+	std::vector<POINT> DrawVertex;
+	DrawVertex.resize(Index->Indexs.size());
+
+	std::vector<float4> CopyBuffer;
+	CopyBuffer.resize(Index->Indexs.size());
+
+
+
+	for (size_t i = 0; i < Index->Indexs.size(); i++)
+	{
+		int TriIndex = Index->Indexs[i];
+
+		// 0 번째 순서의 점이 됩니다.
+		CopyBuffer[i] = Vertex->Vertexs[TriIndex];
+
+		// [0.5f] [0.5f] []                  [100] [100] [] 
+		// 크
+		CopyBuffer[i] *= GetActor()->GetTransform().GetScale();
+
+		// 자전
+		//CopyBuffer[i] *= GetActor()->GetTransform().GetRotation();
+
+		// 이동
+		CopyBuffer[i] += GetActor()->GetTransform().GetPosition();
+
+		
+
+		DrawVertex[i] = CopyBuffer[i].GetConvertWindowPOINT();
+	}
+
+	for (size_t i = 0; i < Vertex->Vertexs.size(); i++)
+	{
+		Vertex->Vertexs[i] = float4::VectorRotationToRadianZ(Vertex->Vertexs[i], _DeltaTime * 10);
+	}
+	
+	for (size_t i = 0; i < DrawVertex.size(); i += 3)
+	{
+		Polygon(GameEngineWindow::GetHDC(), &DrawVertex[i], 3);
+	}
+	
+
+	// Rectangle(GameEngineWindow::GetHDC(), LeftTop.ix(), LeftTop.iy(), RightBot.ix(), RightBot.iy());
+}
